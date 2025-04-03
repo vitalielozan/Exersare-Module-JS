c59f7379231f6f44ba7ab3f6581c43efacbf9d13
vitalielozan <vitalielozan17@outlook.com>
2025-04-03 16:46:47 +0200

    some commit

--- app.js
+++ app.js
@@ -5,59 +5,67 @@ import Motocicleta from './motocicleta.mjs';
 const flotaMea = new Flota();
 
 function afiseazaFlota() {
-    const parcareAuto = document.querySelector('#parcareAuto');
-    parcareAuto.innerHTML = "";
+  const parcareAuto = document.querySelector('#parcareAuto');
+  parcareAuto.innerHTML = '';
 
-    if (flotaMea.vehicule.length === 0) {
-        parcareAuto.innerHTML = "<p>Parcarea este goala...</p>";
-        return;
-    }
-    flotaMea.vehicule.forEach(vehicul => {
-        const vechiculElement = document.createElement("p");
-        vechiculElement.textContent = vehicul.descriere();
-        parcareAuto.appendChild(vechiculElement);
-    });
+  if (flotaMea.vehicule.length === 0) {
+    parcareAuto.innerHTML = '<p>Parcarea este goala...</p>';
+    return;
+  }
+  flotaMea.vehicule.forEach((vehicul) => {
+    const vechiculElement = document.createElement('p');
+    vechiculElement.textContent = vehicul.descriere();
+    parcareAuto.appendChild(vechiculElement);
+  });
 }
-
-const btnRemove = document.querySelector("#btnRemove");
-btnRemove.addEventListener("click", () => {
-    flotaMea.delFromLocalStorage();
-    flotaMea.vehicule = [];
-    flotaMea.toLocalStorage();
-    afiseazaFlota();
-});
 
 const vehiculForm = document.querySelector('#vehiculForm');
 vehiculForm.addEventListener('submit', (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const marca = document.querySelector('#marca').value;
-    const model = document.querySelector('#model').value;
-    const an = parseInt(document.querySelector('#an').value);
-    const tip = document.querySelector('#tip').value;
-    const extra = document.querySelector('#extra').value;
+  const marca = document.querySelector('#marca').value;
+  const model = document.querySelector('#model').value;
+  const an = parseInt(document.querySelector('#an').value);
+  const tip = document.querySelector('#tip').value;
+  const extra = document.querySelector('#extra').value;
 
-    let vehicul;
-    if (tip === 'masina') {
-        if (isNaN(extra)) {
-            vehicul = new Masina(marca, model, an, extra);
-            flotaMea.adaugaVehicul(vehicul);
-        } else {
-            alert('Valoarea introdusă pentru caroserie nu este validă. Introduceți date corecte!');
-        }
-    } else if (tip === 'motocicleta') {
-        if (!isNaN(extra)) {
-            vehicul = new Motocicleta(marca, model, an, extra);
-            flotaMea.adaugaVehicul(vehicul);
-        } else {
-            alert('Valoarea introdusă pentru cilindri nu este validă. Introduceți date corecte!');
-        }
+  let vehicul;
+
+  if (tip === 'masina') {
+    if (isNaN(extra)) {
+      vehicul = new Masina(marca, model, an, extra);
+      flotaMea.addDataToJson(vehicul);
+      flotaMea.adaugaVehicul(vehicul);
     } else {
-        alert('Tipul de vehicul nu este recunoscut.');
+      alert(
+        'Valoarea introdusă pentru caroserie nu este validă. Introduceți date corecte!'
+      );
     }
+  } else if (tip === 'motocicleta') {
+    if (!isNaN(extra)) {
+      vehicul = new Motocicleta(marca, model, an, extra);
+      flotaMea.addDataToJson(vehicul);
+      flotaMea.adaugaVehicul(vehicul);
+    } else {
+      alert(
+        'Valoarea introdusă pentru cilindri nu este validă. Introduceți date corecte!'
+      );
+    }
+  } else {
+    alert('Tipul de vehicul nu este recunoscut.');
+  }
 
-    afiseazaFlota();
-    document.querySelector('#vehiculForm').reset();
+  afiseazaFlota();
+  document.querySelector('#vehiculForm').reset();
+});
+
+const btnRemove = document.querySelector('#btnRemove');
+btnRemove.addEventListener('click', async () => {
+  flotaMea.delFromLocalStorage();
+  await flotaMea.deleteAllVehicul();
+  flotaMea.vehicule = [];
+  flotaMea.toLocalStorage();
+  afiseazaFlota();
 });
 
 afiseazaFlota();
--- flota.mjs
+++ flota.mjs
@@ -1,39 +1,80 @@
 import Masina from './masina.mjs';
 import Motocicleta from './motocicleta.mjs';
+import Vehicul from './vehicul.mjs';
 
 export default class Flota {
-    constructor() {
-        this.vehicule = this.fromLocalStorage() || [];
+  constructor() {
+    this.vehicule = this.fromLocalStorage() || [];
+  }
+
+  adaugaVehicul(vehicul) {
+    this.vehicule.push(vehicul);
+    this.toLocalStorage();
+  }
+
+  toLocalStorage() {
+    localStorage.setItem('flota', JSON.stringify(this.vehicule));
+  }
+
+  async addDataToJson(vehicul) {
+    try {
+      const formData = vehicul;
+      const response = await fetch('http://localhost:4000/vehiculs', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+      console.log('Date salvate');
+      return data;
+    } catch (error) {
+      console.error('Ereare', error);
     }
+  }
 
-    adaugaVehicul(vehicul) {
-        this.vehicule.push(vehicul);
-        this.toLocalStorage();
+  async deleteAllVehicul() {
+    try {
+      const response = await fetch('http://localhost:4000/vehiculs');
+      const vehiculs = await response.json();
+
+      await Promise.all(vehiculs.map((v) => this.deleteDataFromJson(v.id)));
+
+      alert('Toate vehiculele au fost eliminate din baza de date');
+    } catch (error) {
+      console.error('Eroare la ștergerea vehiculelor', error);
     }
+  }
 
-    toLocalStorage() {
-        localStorage.setItem('flota', JSON.stringify(this.vehicule));
+  async deleteDataFromJson(id) {
+    try {
+      await fetch(`http://localhost:4000/vehiculs/${id}`, {
+        method: 'DELETE',
+      });
+    } catch (error) {
+      console.log('Error', error);
     }
+  }
 
+  delFromLocalStorage() {
+    localStorage.removeItem('flota');
+  }
 
-    delFromLocalStorage() {
-        localStorage.removeItem("flota");
-    }
-
-    fromLocalStorage() {
-        const dateSalvate = localStorage.getItem('flota');
-        if (dateSalvate) {
-            const vehiculeJSON = JSON.parse(dateSalvate);
-            return vehiculeJSON.map(v => {
-                if (v.tipCaroserie !== undefined) {
-                    return Masina.fromJSON(v);
-                } else if (v.cilindri !== undefined) {
-                    return Motocicleta.fromJSON(v);
-                } else {
-                    return Vehicul.fromJSON(v);
-                }
-            });
+  fromLocalStorage() {
+    const dateSalvate = localStorage.getItem('flota');
+    if (dateSalvate) {
+      const vehiculeJSON = JSON.parse(dateSalvate);
+      return vehiculeJSON.map((v) => {
+        if (v.tipCaroserie !== undefined) {
+          return Masina.fromJSON(v);
+        } else if (v.cilindri !== undefined) {
+          return Motocicleta.fromJSON(v);
+        } else {
+          return Vehicul.fromJSON(v);
         }
-        return [];
+      });
     }
+    return [];
+  }
 }
